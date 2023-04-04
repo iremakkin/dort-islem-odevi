b4b009f9fd5469e11df81b365083c40462df8af9
İrem Akkın <iremakkin1453@gmail.com>
2023-04-04 16:16:16 +0300

    calculator fonksiyonu oluşturuldu
    içerisinde gerekli hesaplamaları yapıp sonucu döndürür

--- Main.java
+++ Main.java
@@ -15,5 +15,35 @@ public class Main {
     static int randomThreeDigit(){
         return (int)(math.random * 999 + 1);
     }
-    
+
+    static int calculator(int n1, int n2, String operator){
+
+        int sonuc;
+
+        switch (operator){
+            case "+":
+                sonuc = n1 + n2;
+                break;
+            case "-":
+                if(n1 >= n2){
+                    sonuc = n1-n2;}
+                else sonuc = n2-n1;
+                break;
+            case "*":
+                sonuc = n1 * n2;
+                break;
+            case "/":
+                if(n1 >= n2){
+                    sonuc = (int)(n1/n2);}
+                else sonuc = (int)(n2/n1);
+                break;
+            default:
+                System.out.println("Gecersiz secim");
+                sonuc = -1;
+                break;
+        }
+
+        return sonuc;
+    }
+
 }
