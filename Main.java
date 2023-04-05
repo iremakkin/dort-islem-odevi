cfca7690289cf47a51895029c320232f74044f5c
İrem Akkın <iremakkin1453@gmail.com>
2023-04-05 17:43:06 +0300

    Gerekli işlem ve döngülerin yapıldığı ana döngü

--- Main.java
+++ Main.java
@@ -1,10 +1,13 @@
-import java.util.*;
+import java.util.Scanner;
+import java.math.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         String operator;
+        int finalNum = 0;
         int newNum;
+        int kalan=0;
         int[] selectedNums = new int[2];
         int[] oneDigitNum = new int[5];
         int[] availibleNums = new int[6];
@@ -24,6 +27,66 @@ public class Main {
 
         System.out.println(twoDigitNum);
         System.out.println("with these numbers, try to get close the number " + threeDigitNum + " using the operators of (+, - , *, /). ");
+
+        while (areAvailibleNumbersExist) {
+
+                System.out.print("select one of the numbers: ");
+                selectedNums[0] = input.nextInt;
+                System.out.println(" ");
+                System.out.print("select another number: ");
+                selectedNums[1] = input.nextInt;
+                System.out.println(" ");
+                System.out.print("select an operator one of +,-,*,/");
+                operator = input.nextLine;
+                newNum = calculator(selectedNums[0], selectedNums[1], operator);
+
+                for (int i=0; i<6 ; i++){
+                    if(selectedNums[0] == availibleNums[i]){
+                        availibleNums[i] = 0;
+                        break;
+                    }
+                }
+                for (int i=0; i<6 ; i++){
+                    if(selectedNums[1] == availibleNums[i]){
+                        availibleNums[i] = 0;
+                        break;
+                    }
+                }
+                for (int i=0; i<6 ; i++){
+                    if(availibleNums[i] == 0){
+                        availibleNums[i] = newNum;
+                        break;
+                    }
+                }
+
+                System.out.println(" ");
+                System.out.print("numbers that left: ");
+
+                for (int i=0; i<6 ; i++){
+                    if(availibleNums[i]!=0){
+                        System.out.print(availibleNums[i]+ " ");
+                    }
+                }
+
+
+
+                for (int i=0; i<6 ; i++){
+                    if(availibleNums[i]!=0){
+                        kalan++;
+                    }
+                }
+
+                if (kalan == 1){
+
+                    for (int i=0; i<6 ; i++){
+                        if(availibleNums[i]!=0){
+                            finalNum = availibleNums[i];
+                        }
+                    }
+                    areAvailibleNumbersExist = false;
+                }
+        }
+
 
 
 
@@ -48,6 +111,7 @@ public class Main {
     static int randomThreeDigit(){
         return (int) (math.random() * 899 + 100);
     }
+
 
     static int calculator(int n1, int n2, String operator){
 
